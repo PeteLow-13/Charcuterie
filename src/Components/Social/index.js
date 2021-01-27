a8c7fdd0846d80023413b4e68b4e07e7874f09e7
Pete Low <pedritolow@gmail.com>
2021-01-26 17:13:20 -0800

    added social

---
+++ src/Components/Social/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './style.css';
+
+function Social() {
+    return (
+        <div className='social-div'>
+            <h2>Chsck us out on Social</h2>
+            <div>
+            <a target='blank' href='' alt='facebook'>facebook</a>
+            <a target='blank' href='' alt='instagram'>Instagram</a>
+            <a target='blank' href='' alt='yelp'>Yelp</a>
+            </div>
+        </div>
+    )
+}
+
+export default Social
