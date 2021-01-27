301f357690a06f8c6c92c9908a4014dc06b52625
Pete Low <pedritolow@gmail.com>
2021-01-26 17:27:22 -0800

    added gallery

---
+++ src/Components/Gallery/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Gallery() {
+    return (
+        <div className='gallery-div'>
+            <h2>Gallery</h2>
+        </div>
+    )
+}
+
+export default Gallery className='gallery-div'
