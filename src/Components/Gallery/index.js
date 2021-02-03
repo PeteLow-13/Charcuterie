18f8b5c062a87b8b9fe703d989e5b6fe43c8f716
Pete Low <pedritolow@gmail.com>
2021-02-02 22:05:49 -0800

    Working on slide show

--- src/Components/Gallery/index.js
+++ src/Components/Gallery/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Slideshow from '../Slideshow';
 import './style.css';
 
 function Gallery() {
     return (
         <div className='gallery-div'>
             <h2>Gallery</h2>
+            <Slideshow />
         </div>
     )
 }
--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -4,7 +4,7 @@ import './style.css';
 export default function Header() {
     return (
         <div className='header-div'>
-            <h1>L-D Charcuterie</h1>
+            <h1>L&D Charcuterie</h1>
             <h2>Making your events special since 2021</h2>
         </div>
     )
