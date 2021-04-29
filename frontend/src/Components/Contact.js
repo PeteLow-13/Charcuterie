a50b775e372918798d7a871be0b3472039de6414
Pete Low <pedritolow@gmail.com>
2021-04-28 21:48:00 -0700

    Seperated contact and contact form

---
+++ frontend/src/Components/Contact.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Contact = () => {
+    return (
+      <div className='contact-div'>
+        <h2 className='text-center'>Contact Us</h2>
+        <p className='text-center'>Phone: <a href='tel:+12065555555'>(206)-555-5555</a></p>
+        <p className='text-center'>Email: <a href='mailto:ldcharcuterie@gmail.com'>ldcharcuterie@gmail.com</a></p>
+      </div>
+    )
+}
+
+export default Contact
