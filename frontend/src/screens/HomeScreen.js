6a746dc15aed5261496d34cde268801e0d058778
Pete Low <pedritolow@gmail.com>
2021-04-28 21:52:03 -0700

    Added menu link to home screen

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,16 +1,16 @@
-import React from 'react'
+import React from 'react';
 import Social from '../Components/Social';
-
 import Contact from '../Components/Contact';
 import Header from '../Components/Header';
 import CarouselComp from '../Components/CarouselComp';
-// import { Carousel } from 'react-bootstrap';
+import MenuLink from '../Components/MenuLink';
 
 const HomeScreen = () => {
     return (
         <div>
             <Header />
             <CarouselComp />
+            <MenuLink />
             <Social />
             <Contact />
         </div>
