d47046229c2277c117e2ebebf31669c2b302a6d2
Daniel.Yu <adam741963@gmail.com>
2021-05-03 17:16:08 -0700

    redux basic set up: actions, reducers, store

--- frontend/src/Components/Footer/index.js
+++ frontend/src/Components/Footer/index.js
@@ -1,18 +1,34 @@
-import React from 'react'
+import React from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
 
 const Footer = () => {
-    return (
-        <footer>
-            <Container>
-                <Row><Col className='text-center py-3'>Copyright &copy; L&D Charcuterie</Col>
-                </Row>
+  return (
+    <footer>
+      <Container>
+        <Row>
+          <Col className='text-center py-3'>
+            Copyright &copy; L&D Charcuterie
+          </Col>
+        </Row>
 
-                <Row><Col className='text-center py-3'><p>Built by <a href='https://github.com/PeteLow-13' target='blank'>Pete Low 2021</a></p></Col>
-                </Row>
-            </Container>
-        </footer>
-    )
-}
+        <Row>
+          <Col className='text-center py-3'>
+            <p>
+              Built by{' '}
+              <a href='https://github.com/PeteLow-13' target='blank'>
+                Pete Low
+              </a>
+              {' & '}
+              <a href='https://github.com/DanielYu0864' target='blank'>
+                {'Daniel Yu'}
+              </a>
+              {' 2021'}
+            </p>
+          </Col>
+        </Row>
+      </Container>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
--- frontend/src/screens/MenuScreen.js
+++ frontend/src/screens/MenuScreen.js
@@ -1,35 +1,40 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Row, Col } from 'react-bootstrap';
 import foodItems from '../foodItems';
 import MenuCard from '../Components/MenuCard';
 import axios from 'axios';
-import { Row, Col } from 'react-bootstrap';
+import { listProducts } from '../actions/productActions';
+import Loader from '../Components/Loader';
+import Message from '../Components/Message';
 
 const MenuScreen = () => {
-  const [products, setProducts] = useState([]);
+  const dispatch = useDispatch();
+
+  const productList = useSelector((state) => state.productList);
+
+  const { loading, error, products } = productList;
 
   useEffect(() => {
-    const fetchProducts = async () => {
-      const { data } = await axios.get('/api/products');
-
-      setProducts(data);
-      // console.log(data)
-    };
-
-    fetchProducts();
-
-    // console.log(products)
-  }, []);
+    dispatch(listProducts());
+  }, [dispatch]);
 
   return (
     <>
       <h1>Menu Offerings</h1>
-      <Row>
-        {foodItems.map((menuItem) => (
-          <Col key={menuItem._id} sm={12} md={6} lg={4} xl={3}>
-            <MenuCard menuItem={menuItem} />
-          </Col>
-        ))}
-      </Row>
+      {loading ? (
+        <Loader />
+      ) : error ? (
+        <Message variant='danger'>{error}</Message>
+      ) : (
+        <Row>
+          {products.map((menuItem) => (
+            <Col key={menuItem._id} sm={12} md={6} lg={4} xl={3}>
+              <MenuCard menuItem={menuItem} />
+            </Col>
+          ))}
+        </Row>
+      )}
     </>
   );
 };
