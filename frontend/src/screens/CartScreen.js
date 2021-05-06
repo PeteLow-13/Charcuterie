dbab92d758de7b11ef37190065939cb2152ab181
Daniel.Yu <adam741963@gmail.com>
2021-05-06 16:38:06 -0700

    cart

---
+++ frontend/src/screens/CartScreen.js
@@ -0,0 +1,26 @@
+import React, { useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import {
+  Row,
+  Col,
+  ListGroup,
+  Image,
+  Form,
+  Button,
+  Card,
+} from 'react-bootstrap';
+import Message from '../components/Message';
+
+/*
+!react-router:
+*match -> https://reactrouter.com/web/api/match
+*loaction -> https://reactrouter.com/web/api/location
+*history -> https://reactrouter.com/web/api/history
+*/
+
+const CartScreen = () => {
+  return <div></div>;
+};
+
+export default CartScreen;
