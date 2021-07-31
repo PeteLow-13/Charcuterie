8bc8a111fb33e4e351c40db055107ed382cf9866
Pete Low <pedritolow@gmail.com>
2021-07-31 16:13:33 -0700

    added success to dependencies

--- frontend/src/screens/ProfileScreen.js
+++ frontend/src/screens/ProfileScreen.js
@@ -44,7 +44,7 @@ const ProfileScreen = ({ location, history }) => {
         setEmail(user.email);
       }
     }
-  }, [dispatch, history, userInfo, user]);
+  }, [dispatch, history, userInfo, user, success]);
 
   const submitHandler = (e) => {
     e.preventDefault();
