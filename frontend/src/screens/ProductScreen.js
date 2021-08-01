fde81ce2dc417a3dd6f22c85be69cc8d23866b2c
Pete Low <pedritolow@gmail.com>
2021-08-01 15:08:31 -0700

    Rounded image on ProductScreen

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -44,7 +44,12 @@ const ProductScreen = ({ history, match }) => {
         <>
           <Row>
             <Col md={6}>
-              <Image src={product.image} alt={product.name} fluid />
+              <Image
+                src={product.image}
+                alt={product.name}
+                fluid
+                className='rounded'
+              />
             </Col>
             <Col md={3}>
               <ListGroup variant='flush'>
